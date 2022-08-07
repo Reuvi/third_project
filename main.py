3f06d922d0ceb15019a5420c48bca56790d3b9d9
Reuvi <reuvi_israeli@icloud.com>
2022-08-07 12:48:52 -0400

    Second Commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame, sys
+from settings import *
+from tiles import Tile
+from level import Level
+# Pygame setup
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+level = Level(level_map, screen)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.ext()
+
+    screen.fill('black')
+
+    pygame.display.update()
+    clock.tick(60)
